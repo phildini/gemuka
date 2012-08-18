68a1c022c0ab33ffe7553c3b8104721185d4daa9
Philip James <philip@eventbrite.com>
2012-08-18 14:00:55 -0700

    Updated the settings and the accounts model.

--- gemuka/settings.py
+++ gemuka/settings.py
@@ -41,21 +41,7 @@ try:
 except:
     print "Unexpected error:", exc_info()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'django',                      # Or path to database file if using sqlite3.
-#         'USER': '',                      # Not used with sqlite3.
-#         'PASSWORD': '',                  # Not used with sqlite3.
-#         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
@@ -154,6 +140,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
 )
 
+AUTH_PROFILE_MODULE = 'gemuka.accounts.UserProfile'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -162,12 +149,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'gemuka.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'south',
     'gunicorn',
+    'gemuka.accounts',
 )
 
 BROKER_BACKEND = 'django'
